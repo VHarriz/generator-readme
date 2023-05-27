98426287b88e3d79145a1bc023bf99ef8a2fe35e
VHarriz <128908597+VHarriz@users.noreply.github.com>
2023-05-27 11:04:09 -0400

    generator save

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,59 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs =require('fs');
+const generateMD = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptQ = [
+    {
+        type: 'input',
+        message: 'What is your project title name?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Write a little about what your project is suppose to do.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'How do you install this project?',
+        name: 'install'
+    },
+    {
+        type: 'list',
+        message: 'Select license used.',
+        name: 'license',
+        choices:['MIT', 'Apache', 'GPL', 'None']
+    },
+    {
+        type: 'input',
+        message: 'Enter Github username:',
+        name: 'username'
+    }
+
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(promptQ)
+    .then((data) => {
+       return generateMD(data);
+    })
+    .then((data) => {
+        writeToFile('README.md', data); 
+    })
+
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if ( license === 'MIT') {
+    return 'MIT License Badge';
+  } else if (license === 'Apache') {
+    return 'Apache License Badge';
+  } else if ( license === 'GPL') {
+    return 'GPL License Badge';
+  } else if ( license === 'None') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if ( license === 'MIT') {
+    return 'MIT License Link';
+  } else if (license === 'Apache') {
+    return 'Apache License Link';
+  } else if ( license === 'GPL') {
+    return 'GPL License Link';
+  } else if ( license === 'None') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Installation Instructions
+  ${data.install}
+
+  ## License
+  ${data.license}
+
+  Github Username
+  ${data.username}
 
 `;
 }
